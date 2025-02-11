b2b03d3aa2b908b4646599587e2918223482aff9
Kivthor <50101494+Kivthor@users.noreply.github.com>
2025-02-11 16:32:51 +0700

    kek

---
+++ src/components/UiDateInput.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  type?: string
+  name: string
+  modelValue: string
+}>()
+
+const emit = defineEmits(['update:model-value'])
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:model-value', target.value)
+}
+</script>
+
+<template>
+  <input :type="type" :name="name" :value="modelValue" class="todoItemDateInput" @input="onInput" />
+</template>
+
+<style scoped>
+.todoItemDateInput {
+  padding: 0 0.5rem;
+  text-transform: uppercase;
+  font-family: monospace;
+  background-color: var(--main-color-lighter);
+  color: var(--main-color-text);
+  color-scheme: dark;
+  border: var(--border-button);
+  border-radius: var(--main-radius);
+  transition: 0.2s;
+  &:hover {
+    background-color: var(--main-color-darker);
+    border-color: var(--main-color-darker);
+  }
+  &:active {
+    background-color: var(--main-color-very-light);
+    border-color: var(--main-color-very-light);
+  }
+}
+</style>
---
+++ src/components/UiTextArea.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+interface Props {
+  name: string
+  placeholder?: string
+  maxlength?: number
+  rows?: number
+  modelValue: string
+}
+
+withDefaults(defineProps<Props>(), {
+  placeholder: 'default',
+})
+
+const emit = defineEmits(['update:model-value'])
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:model-value', target.value)
+}
+</script>
+
+<template>
+  <textarea
+    :name="name"
+    :placeholder="placeholder"
+    :rows="rows"
+    :maxlength="maxlength"
+    :value="modelValue"
+    class="todoItemTextInput"
+    @input="onInput"
+  />
+</template>
+
+<style scoped>
+.todoItemTextInput {
+  display: block;
+  width: 100%;
+  padding: 0;
+  font-size: 1rem;
+  letter-spacing: normal;
+  word-spacing: normal;
+  resize: none;
+  background-color: var(--main-color);
+  color: var(--main-color-text);
+  color-scheme: dark;
+  border: var(--border-sub-container);
+}
+</style>

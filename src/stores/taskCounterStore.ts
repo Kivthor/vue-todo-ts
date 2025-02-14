9461ca304e12fa3e339c556894743879ba382d1c
Kivthor <50101494+Kivthor@users.noreply.github.com>
2025-02-14 16:16:43 +0700

    tasks are now being saved to localstorage

--- src/stores/taskCounterStore.ts
+++ src/stores/taskCounterStore.ts
@@ -1,11 +1,23 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
+const localTaskCounter: string | null = localStorage.getItem('taskCounter')
+
 export const useTaskCounterStore = defineStore('taskCounter', () => {
   const taskCount = ref(0)
-  const addTask = () => taskCount.value++
-  const deleteTask = () => taskCount.value--
-  const clearTasks = () => (taskCount.value = 0)
+  taskCount.value = localTaskCounter !== null ? +JSON.parse(localTaskCounter) : 0
+  const addTask = () => {
+    taskCount.value++
+    localStorage.setItem('taskCounter', JSON.stringify(taskCount.value))
+  }
+  const deleteTask = () => {
+    taskCount.value--
+    localStorage.setItem('taskCounter', JSON.stringify(taskCount.value))
+  }
+  const clearTasks = () => {
+    taskCount.value = 0
+    localStorage.removeItem('taskCounter')
+  }
   return {
     taskCount,
     addTask,
--- src/stores/taskListStore.ts
+++ src/stores/taskListStore.ts
@@ -9,8 +9,12 @@ interface Task {
   status: 'WAITING' | 'SAVED' | 'NOT-SAVED'
 }
 
+const localTaskList: string | null = localStorage.getItem('taskList')
+
 export const useTaskListStore = defineStore('taskListStore', () => {
   const taskList = ref<Task[]>([])
+  taskList.value = localTaskList !== null ? JSON.parse(localTaskList) : []
+
   const addTask = () => {
     taskList.value.push({
       id: 'id' + Math.floor(Math.random() * 1000),
@@ -19,9 +23,11 @@ export const useTaskListStore = defineStore('taskListStore', () => {
       date: '',
       status: 'WAITING',
     })
+    localStorage.setItem('taskList', JSON.stringify(taskList.value))
   }
   const deleteTask = (taskId: string) => {
     taskList.value = taskList.value.filter((task) => task.id !== taskId)
+    localStorage.setItem('taskList', JSON.stringify(taskList.value))
     console.log(`DELETED TASK WITH ID === ${taskId}`)
   }
   const modTask = (
@@ -39,9 +45,11 @@ export const useTaskListStore = defineStore('taskListStore', () => {
         task.status = isSaved ? 'SAVED' : 'NOT-SAVED'
       }
     }
+    localStorage.setItem('taskList', JSON.stringify(taskList.value))
   }
   const clearTaskList = () => {
     taskList.value = []
+    localStorage.removeItem('taskList')
     console.log('CLEAR ALL TASKS')
   }
   return {
